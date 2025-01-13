ca0e20d174d052ab4afb62ec7f9fb86c790c40db
ytnom00 <monuyaduvanshi808@gmail.com>
2025-01-13 06:01:04 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+let btn=document.querySelector("#mode");
+let curmode="light";
+btn.addEventListener("click",()=>{
+    if(curmode==="light"){
+        document.body.classList.add("dark") ;
 
+    curmode="dark";
+    document.body.classList.remove("light");
+}
+else if(curmode=="dark"){
+
+    document.body.classList.add("light");
+    curmode="light";
+    document.body.classList.remove("dark");
+}
+console.log(curmode);
+});
+                            
